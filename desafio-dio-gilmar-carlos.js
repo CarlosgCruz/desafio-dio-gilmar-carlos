a6c88194ecbf38f8be72f684547d2bb21ceb89f8
ggdosj <gilmarcarlos711@gmail.com>
2023-11-05 17:14:29 -0400

    commit

---
+++ desafio-dio-gilmar-carlos.js
@@ -0,0 +1,23 @@
+const nomeDoHeroi = " GG THE DEV";
+
+const xpDoHeroi = 100000;
+
+
+if (xpDoHeroi <= 1000) {
+  console.log("O heroi de nome" + nomeDoHeroi + " está nível de Ferro.");
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+  console.log("O heroi de nome" + nomeDoHeroi + " está nível de Bronze.");
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+  console.log("O heroi de nome" + nomeDoHeroi + " está nível de Prata.");
+} else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000) {
+  console.log("O heroi de nome" + nomeDoHeroi + " está nível de Ouro.");
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+  console.log("O heroi de nome" + nomeDoHeroi + " está nível de Platina.");
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+  console.log("O heroi de nome" + nomeDoHeroi + " está nível de Ascendente.");
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+  console.log("O heroi de nome" + nomeDoHeroi + " está nível de Imortal.");
+} else {
+  console.log("O heroi de nome" + nomeDoHeroi + " está nível de Radiante.");
+}
+
